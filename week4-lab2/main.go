2a12434f724d9131aa72c2cb8902942011c87126
SopitaJ <jengsriwong_s@su.ac.th>
2025-08-06 06:50:03 +0000

    week4-lab2

---
+++ week4-lab2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var name string = "Sopita"
+	var age int = 20
+
+	email := "sopitajengsiwong@gmail.com"
+	gpa := 3.51
+	firstname, lastname := "sopita", "jengsiwong"
+	fmt.Printf("Name %s %s , age %d , email %s , gpa %.2f", firstname, lastname, age, email, gpa)
+
+}
